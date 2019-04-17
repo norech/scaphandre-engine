ed0afaec8842f9a3cd450ce4d3ee98e85ad0a26e
Alexis Norech <alexis.norech@gmail.com>
2019-04-18 01:11:40 +0400

    Moved Event class to ScaphandreEngine.Events

--- ScaphandreEngine/Events/Event.cs
+++ ScaphandreEngine/Events/Event.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ScaphandreEngine.EventHandling
+namespace ScaphandreEngine.Events
 {
     public abstract class Event
     {
