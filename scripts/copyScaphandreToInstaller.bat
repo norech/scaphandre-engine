3a1f05182eb8aa5a64f6831528706045e5e09ea6
Alexis Norech <32593294+norech@users.noreply.github.com>
2019-04-17 07:48:45 +0400

    Update copyScaphandreToInstaller.bat

--- scripts/copyScaphandreToInstaller.bat
+++ scripts/copyScaphandreToInstaller.bat
@@ -2,4 +2,5 @@ rmdir /S /Q "%2/ScaphandreInstaller/bin/%1/Scaphandre"
 mkdir "%2/ScaphandreInstaller/bin/%1/Scaphandre"
 
 cd "%2/Assembly-CSharp.ScaphandreEngine.mm/bin/%1"
-xcopy *.dll "%2/ScaphandreInstaller/bin/%1/Scaphandre" /y /d
+xcopy Assembly-CSharp.ScaphandreEngine.mm.dll "%2/ScaphandreInstaller/bin/%1/Scaphandre" /y /d
+xcopy ScaphandreEngine.dll "%2/ScaphandreInstaller/bin/%1/Scaphandre" /y /d
