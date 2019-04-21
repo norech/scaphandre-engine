cc6cb1f76125d35c3632887481e9652fa3d0b4dd
Alexis Norech <alexis.norech@gmail.com>
2019-04-21 16:27:10 +0400

    Added entities

---
+++ ScaphandreEngine/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ScaphandreEngine.Entities
+{
+    public class Entity
+    {
+        public GameObject GameObject { get; }
+
+        public Entity(GameObject gameObject)
+        {
+            GameObject = gameObject;
+        }
+    }
+}
