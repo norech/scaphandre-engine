6565865db1c1d2f1d08a18f0d276b9450480dbf1
Alexis Norech <alexis.norech@gmail.com>
2019-04-27 22:21:58 +0400

    Added story goal completed event

---
+++ Assembly-CSharp.ScaphandreEngine.mm/Story/patch_StoryGoalManager.cs
@@ -0,0 +1,22 @@
+﻿using MonoMod;
+using ScaphandreEngine.EventHandling;
+using ScaphandreEngine.Events;
+
+namespace Story
+{
+    class patch_StoryGoalManager : StoryGoalManager
+    {
+        public extern bool origin_OnGoalComplete(string key);
+
+        [MonoModReplace]
+        private new bool OnGoalComplete(string key)
+        {
+            if(EventManager.TriggerEvent(new StoryGoalCompletedEvent(key)).IsCancelled) {
+                return false;
+            }
+
+            return origin_OnGoalComplete(key);
+        }
+    }
+
+}
---
+++ ScaphandreEngine/Events/StoryGoalCompletedEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScaphandreEngine.Events
+{
+    class StoryGoalCompletedEvent : Event
+    {
+        public StoryGoalCompletedEvent(string key)
+        {
+            Key = key;
+        }
+
+        public string Key { get; }
+
+        public override bool IsCancellable => true;
+    }
+}
