ab9b74ed21abb837e2eef725bf0b33d9b32a4c9e
Alexis Norech <alexis.norech@gmail.com>
2019-04-28 20:42:54 +0400

    Added Assertions System

---
+++ ScaphandreEngine/Assertions/AssertionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScaphandreEngine.Assertions
+{
+    class AssertionException : Exception
+    {
+        public AssertionException(string msg) : base(msg)
+        {
+            //
+        }
+    }
+}
